949fe6ea250e96d86d0a1ca75e624e4be359c06c
lucas <lucasferreirapacoti@gmail.com99056880>
2021-12-13 23:29:49 -0300

    Segunda Submissao

---
+++ Lista_02/src/Proxy/Esqueleto.java
@@ -0,0 +1,43 @@
+package Proxy;
+
+public class Esqueleto {
+
+	public String add(String oper, String valor1, String valor2){
+		Calculadora calc = new Calculadora();
+		Double value1 = Double.parseDouble(valor1);
+		Double value2 = Double.parseDouble(valor2);
+		return  String.valueOf(calc.add(value1,value2));
+				
+	}
+
+
+
+	public String sub(String oper, String valor1, String valor2){
+		Calculadora calc = new Calculadora();
+		Double value1 = Double.parseDouble(valor1);
+		Double value2 = Double.parseDouble(valor2);
+		return String.valueOf(calc.sub(value1,value2));
+
+	}
+
+
+	public String mult(String oper, String valor1, String valor2){
+		Calculadora calc = new Calculadora();
+		Double value1 = Double.parseDouble(valor1);
+		Double value2 = Double.parseDouble(valor2);
+		return  String.valueOf(calc.mult(value1,value2));
+				
+		
+	}
+
+
+	public String div(String oper, String valor1, String valor2){
+		Calculadora calc = new Calculadora();
+		Double value1 = Double.parseDouble(valor1);
+		Double value2 = Double.parseDouble(valor2);
+		return  String.valueOf(calc.div(value1,value2));
+				
+		
+	}
+		
+}
